9bffbdfff4932fc6075f195882165ed3d9de6606
MoziIOnTop <trgiahan732000@gmail.com>
2026-06-19 21:19:54 +0700

    Update gag2main.lua

--- gag2main.lua
+++ gag2main.lua
@@ -1,7 +1,1 @@
-local pid = game.PlaceId
-
-if pid == 133438856880402 or pid == 97598239454123 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/pro/refs/heads/main/gag.lua", true))()
-else
-    game:GetService("Players").LocalPlayer:Kick("Please run this script at Grow a Garden 2\nDiscord.gg/mozil")
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/pro/refs/heads/main/gag.lua", true))()
